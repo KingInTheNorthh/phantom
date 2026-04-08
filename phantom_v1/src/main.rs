98590b08bef3172791b1326447b85d6ff336db0b
KingInTheNorthh <goooogleboy23@gmail.com>
2026-04-08 23:02:20 +0000

    func to read pids from proc

--- phantom_v1/src/main.rs
+++ phantom_v1/src/main.rs
@@ -1,3 +1,31 @@
+use std::fs;
+use std::io;
+
+fn print_pids() -> io::Result<()> {
+
+    let entries = fs::read_dir("/proc")?;
+
+    for entry in entries{
+        let entry = entry?;
+
+        let file_name = entry.file_name();
+        let name_str = file_name.to_string_lossy();
+
+        // filter
+        if name_str.parse::<u32>().is_ok() {
+
+            println!("{}", name_str);
+        }
+
+
+
+    }
+
+   Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = print_pids() {
+        eprintln!("Error reading /proc: {}", e);
+    }
 }
